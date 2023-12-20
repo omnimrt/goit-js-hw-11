cd5050aa5dd18265dd83f4a001aa5f180e83511d
Martin <martinmyhalyna@gmail.com>
2023-12-20 18:33:20 -0500

    done

--- src/main.js
+++ src/main.js
@@ -26,60 +26,63 @@ const hideLoader = () => {
   const loader = document.querySelector('.loader');
   if (loader) {
     loader.remove();
+    console.log('Loader is hidden');
   }
 };
 
 fetchPicturesForm.addEventListener('submit', event => {
+  showLoader();
   event.preventDefault(); // Prevent the default form submission behavior
   gallery.innerHTML = '';
-  showLoader();
   const apiKey = '41249104-77dc8b1e0563744cb8297ef15';
   const query = userInput.value;
 
-  fetch(
-    `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-      query
-    )}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: '',
-          backgroundColor: '#EF4040',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      } else {
-        const markup = data.hits
-          .map(data => {
-            return `<li class="gallery-item"><a href="${data.webformatURL}">
+  setTimeout(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
+        query
+      )}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.hits.length === 0) {
+          iziToast.error({
+            title: '',
+            backgroundColor: '#EF4040',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+          });
+        } else {
+          const markup = data.hits
+            .map(data => {
+              return `<li class="gallery-item"><a href="${data.webformatURL}">
           <img class="gallery-image" src="${data.webformatURL}" alt="${data.tags}"></a>
           <p><b>Likes: </b>${data.likes}</p>
           <p><b>Views: </b>${data.views}</p>
           <p><b>Comments: </b>${data.comments}</p>
           <p><b>Downloads: </b>${data.downloads}</p>
           </li>`;
-          })
-          .join('');
-        gallery.insertAdjacentHTML('afterbegin', markup);
-        const lightbox = new SimpleLightbox('.gallery a', options);
-        lightbox.on('show.simplelightbox');
-        lightbox.refresh();
-        fetchPicturesForm.reset();
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      hideLoader();
-    });
+            })
+            .join('');
+          gallery.insertAdjacentHTML('afterbegin', markup);
+          const lightbox = new SimpleLightbox('.gallery a', options);
+          lightbox.on('show.simplelightbox');
+          lightbox.refresh();
+          fetchPicturesForm.reset();
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        hideLoader();
+      });
+  }, 1000);
 });
 
 const options = {
