82be2a914cca399fd4b8295a0427a0864982eba7
Martin <martinmyhalyna@gmail.com>
2023-12-21 06:04:31 -0500

    deleted logs and setTimeout

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ const containerDiv = document.querySelector('.container');
 
 // Function to show the loader
 const showLoader = () => {
-  console.log('Loader is shown');
   const loader = document.createElement('span');
   loader.classList.add('loader');
   containerDiv.append(loader);
@@ -26,7 +25,6 @@ const hideLoader = () => {
   const loader = document.querySelector('.loader');
   if (loader) {
     loader.remove();
-    console.log('Loader is hidden');
   }
 };
 
@@ -37,52 +35,50 @@ fetchPicturesForm.addEventListener('submit', event => {
   const apiKey = '41249104-77dc8b1e0563744cb8297ef15';
   const query = userInput.value;
 
-  setTimeout(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-        query
-      )}&image_type=photo&orientation=horizontal&safesearch=true`
-    )
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.hits.length === 0) {
-          iziToast.error({
-            title: '',
-            backgroundColor: '#EF4040',
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-          });
-        } else {
-          const markup = data.hits
-            .map(data => {
-              return `<li class="gallery-item"><a href="${data.webformatURL}">
+  fetch(
+    `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
+      query
+    )}&image_type=photo&orientation=horizontal&safesearch=true`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: '',
+          backgroundColor: '#EF4040',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      } else {
+        const markup = data.hits
+          .map(data => {
+            return `<li class="gallery-item"><a href="${data.webformatURL}">
           <img class="gallery-image" src="${data.webformatURL}" alt="${data.tags}"></a>
           <p><b>Likes: </b>${data.likes}</p>
           <p><b>Views: </b>${data.views}</p>
           <p><b>Comments: </b>${data.comments}</p>
           <p><b>Downloads: </b>${data.downloads}</p>
           </li>`;
-            })
-            .join('');
-          gallery.insertAdjacentHTML('afterbegin', markup);
-          const lightbox = new SimpleLightbox('.gallery a', options);
-          lightbox.on('show.simplelightbox');
-          lightbox.refresh();
-          fetchPicturesForm.reset();
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      })
-      .finally(() => {
-        hideLoader();
-      });
-  }, 1000);
+          })
+          .join('');
+        gallery.insertAdjacentHTML('afterbegin', markup);
+        const lightbox = new SimpleLightbox('.gallery a', options);
+        lightbox.on('show.simplelightbox');
+        lightbox.refresh();
+        fetchPicturesForm.reset();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      hideLoader();
+    });
 });
 
 const options = {
